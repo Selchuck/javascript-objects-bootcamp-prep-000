2f58fc68b5ccdc090c16ed79aa8e6547b7c291ae
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-24 11:50:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist (playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist (playlist, artistName) { 
-  delete artistName.songTitle;
+  delete playlist.[artistName]songTitle;
   return updatePlaylist;
 }
